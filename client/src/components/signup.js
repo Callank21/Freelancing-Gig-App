a3a8bb10cb9b539d576e399c618ac3b0c448f291
luisgcodes <lcienfuegos95@gmail.com>
2022-07-24 13:32:31 -0700

    added form

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -3,7 +3,21 @@ export default function Signup() {
     return (
 
     <section className="formz">
-       
+        <div class="containerz" id="containerz">
+
+	        <div class="form-containerz sign-in-containerz">
+		        <form action="#">
+			        <h1 className="log">Sign Up</h1>
+			        
+                    <input type="text" placeholder="Name" />
+			        <input type="email" placeholder="Email" />
+			        <input type="password" placeholder="Password" />
+			        <a href="#/">Forgot your password?</a>
+			
+                    <button>Sign Up</button>
+		        </form>
+	        </div>
+        </div>
     </section>
     
     )
