6cff91317a43232bb34fa73d0358d3c2a8690d64
luisgcodes <lcienfuegos95@gmail.com>
2022-07-24 20:30:56 -0700

    Import Settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,40 @@
 // import logo from './logo.svg';
-import { useState, useEffect } from "react";
+import { useState,  } from "react";
 import  HomePage  from "./components/homePage";
+// import Settings from "./components/settingsPage";
 // import Login from "./components/login";
 // import Signup from "./components/signup";
 // import { Header } from "./components/header";
 // import { Developers } from "./components/developers";
 // import { Contact } from "./components/contact";
-import JsonData from "./data/data.json";
+// import JsonData from "./data/data.json";
 import "./App.css";
 
 
 
 
-
 function App() {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+  const [currentPage, handlePageChange] = useState(" ");
 
+  const renderPage = () => {
+    switch(currentPage) {
+      case "Home Page":
+        return <HomePage  />;
+      // case "Signup":
+      //   return <Signup />;
+      default: 
+    }
+  };
   return (
     <div>
-    <HomePage data={landingPageData.homePage} />
-  </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+      <HomePage currentPage={currentPage} handlePageChange={handlePageChange}/>
+      {renderPage()}
+      
+    </div>   
   );
 }
 
+
+
 export default App;
+
