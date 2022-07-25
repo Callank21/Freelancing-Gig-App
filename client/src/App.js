06dde277fc0291525377c0bd860bc4283c2f31a9
Seungjin <infinissible@gmail.com>
2022-07-25 04:09:48 -0700

    apollo provider added

--- client/src/App.js
+++ client/src/App.js
@@ -6,31 +6,61 @@ import Signup from './components/pages/Signup';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Developers from './components/pages/Developers';
+import NotFound from './components/pages/NotFound';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 // import JsonData from "./data/data.json";
 import './App.css';
 // import CategoryPage from './components/pages/CategoryPage';
 // import ProfilePage from './components/pages/ProfilePage';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Header />
-        <main>
-          {/* <ProfilePage /> */}
-          {/* <CategoryPage /> */}
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/developers" element={<Developers />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </main>
-      </Router>
-      <Footer />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Router>
+          <Header />
+          <main>
+            {/* <ProfilePage /> */}
+            {/* <CategoryPage /> */}
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/developers" element={<Developers />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+        </Router>
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 }
 
