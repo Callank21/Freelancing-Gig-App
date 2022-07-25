6a9db13a3b14c06302dbc7e8474b1c1ab98d6fd2
luisgcodes <lcienfuegos95@gmail.com>
2022-07-24 20:45:52 -0700

    creating divs

--- client/src/components/settingsPage.js
+++ client/src/components/settingsPage.js
@@ -2,7 +2,12 @@ export default function Settings() {
     
     return (
 
-
-
+        <div class="body-root">
+        <div class="body-section">
+          <div class="body-contentz">
+            
+        </div>
+      </div>
+    </div>
  )
 }
