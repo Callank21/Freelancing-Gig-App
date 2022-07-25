38122d4a7dd15e46962dc17250f4f6b5b9638c68
luisgcodes <lcienfuegos95@gmail.com>
2022-07-24 20:47:11 -0700

    forms

--- client/src/components/settingsPage.js
+++ client/src/components/settingsPage.js
@@ -1,11 +1,62 @@
 export default function Settings() {
     
     return (
-
         <div class="body-root">
         <div class="body-section">
           <div class="body-contentz">
-            
+            <div class="mainz">
+              <div class="mainz-content">
+                <div class="mainz-container account-settings">
+                  <h2 className="h2z">Account Settings</h2>
+                  <div class="settings-form account">
+                    <section>
+                    <form class="account-settings-form" id="formzz">
+                      <div class="form-field field-1 short">
+                        <label for="platform-name">My Email Address:</label>
+                        <span> (Users Email) </span>
+                      </div>
+                      
+              
+                      <div class="settings-actions">
+                        <div class="button-container">
+                          <button class="close-account" id="btn" type="submit">Close My Account</button>
+                        </div>
+                      </div>
+                    </form>
+                    </section>
+          
+                    
+                  </div>
+                </div>          
+                
+                           <div class="mainz-container account-settings">
+                  <h2 className="h2z">Changle Email & Password</h2>
+                  <div class="settings-form account">
+                    <section>
+                    <form class="account-settings-form" id="formzz">
+                      <div class="form-field field-1 short">
+                        <label id="labelz" for="platform-name">Email Address</label>
+                        <input id="platform-name inputz" type="text" name="platform-name" />
+                      </div>
+                      
+                      <div class="form-field field-4 short">
+                        <label id="labelz" for="contact-email">Password </label>
+                        <input id="contact-email inputz" type="text" name="contact-email" />
+                                                                                         
+                      </div>
+                      <div class="settings-actions">
+                        <div class="button-container">
+                         <button class="save-settings" id="btn" type="submit">Save settings</button>
+                        </div>
+                      </div>
+                    </form>
+                    </section>
+          
+                    
+                  </div>
+                </div>  
+            </div>
+          </div>
         </div>
       </div>
     </div>
