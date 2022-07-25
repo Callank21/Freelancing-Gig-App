c9f28c2be673c0eb00d80ee074c3af2be8cfe72d
luisgcodes <lcienfuegos95@gmail.com>
2022-07-24 20:40:18 -0700

    Fixed classes

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -2,33 +2,28 @@ export default function Signup() {
 
     return (
 
-    <section className="formz">
+<section className="formz">
         <div class="containerz" id="containerz">
 
-	        <div class="form-containerz sign-in-containerz">
-		        <form action="#">
-			        <h1 className="log">Sign Up</h1>
-			        
-                    <input type="text" placeholder="Name" />
-			        <input type="email" placeholder="Email" />
-			        <input type="password" placeholder="Password" />
-			        <a href="#/">Forgot your password?</a>
-			
-                    <button>Sign Up</button>
-		        </form>
-	        </div>
-
-	        <div class="overlay-container">
-		        <div class="overlay">
-			        <div class="overlay-panel overlay-right">
-				        <h1 className="logz">Welcome Back!</h1>
-				        <p className="ppp">Enter your personal details and start your journey!!</p>
-			        </div>
-		        </div>
-	        </div>
-            
-        </div>
-    </section>
-    
-    )
+	<div class="form-containerz sign-in-containerz">
+	<form action="#" id="login">
+			<h1 className="log">Sign Up</h1>
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<a href="#/" className="forgot-password">Forgot your password?</a>
+			<button className="buttonz">Sign Up</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-right">
+				<h1 className="logz">Welcome !</h1>
+				<p className="ppp">Enter your personal details and start your journey!!</p>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+	)
 }
