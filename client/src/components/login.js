3f777f180bbfeec783a4b476b2db9f22cfbf795f
luisgcodes <lcienfuegos95@gmail.com>
2022-07-24 20:32:23 -0700

    Fixed Classes

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,33 +1,31 @@
 export default function Login() {
     
     return (
-    <section className="formz">
+        <section className="formz">
         <div class="containerz" id="containerz">
 
-	        <div class="form-containerz sign-in-containerz">
-		        <form action="#">
-			        <h1 className="log">Sign in</h1>
+	<div class="form-containerz sign-in-containerz">
+		<form action="#" id="login">
+			<h1 className="log">Sign in</h1>
 			
-			        <input type="email" placeholder="Email" />
-			        <input type="password" placeholder="Password" />
-			        
-                    <a href="#/">Forgot your password?</a>
-			        
-                    <button>Sign In</button>
-		        </form>
-	        </div>
-	    <div class="overlay-container">
-		    <div class="overlay">
-			    <div class="overlay-panel overlay-right">
-				    <h1 className="logz">Welcome Back!</h1>
-				    <p className="ppp">Enter your personal details and continue your journey!</p>
-			    </div>
-		    </div>
-	    </div>
-        </div>
-    </section>
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<a href="#/" className="forgot-password">Forgot your password?</a>
+			<button className="buttonz">Sign In</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-right">
+				<h1 className="logz">Welcome Back!</h1>
+				<p className="ppp">Enter your personal details and continue your journey!</p>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
 
-    )
-}
+         )
+       }
 
 
