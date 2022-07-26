330280fe013769ae9bac8dbc682f2a37b45bdbd2
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-07-25 20:00:19 -0700

    moved category page code to profile page, userProfile started

--- client/src/components/pages/UserProfile.js
+++ client/src/components/pages/UserProfile.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React from 'react';
 
-export const UserProfile = (props) => {
-
-};
+const UserProfile = (props) => {
+	const { username: userParam } = useParams();
+	const { loading, data } = useQuery(userParam ? QUERY_Profile {
+	  variables: { username: userParam },
+	});
+  
+	const user = data?.me || data?.user || {};
+  
+	if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+	  return <Navigation ="/profile" />;
+	}
+  
+	if (loading) {
+	  return <div>Loading...</div>;
+	}
+  
+	if (!user?.username) {
+	  return (
+		<h4> Please sign up or log in </h4>
+	  );
+	}
+	
