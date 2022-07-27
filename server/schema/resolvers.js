52b6d753aa45af21e7bc294ee8d62b00ff8a577f
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-07-26 23:58:09 -0700

    changes

---
+++ server/schema/resolvers.js
@@ -0,0 +1,121 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Profile } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('profile');
+
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+
+    users: async () => {
+      return User.find().select('-__v -password').populate('profile');
+    },
+
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('profile');
+    },
+
+    profiles: async () => {
+      return Profile.find();
+    },
+
+    profile: async (parent, { _id }) => {
+      return Profile.findOne({ _id });
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    createProfile: async (parent, args, context) => {
+      if (context.user) {
+        const profile = await Profile.create({
+          ...args,
+          username: context.user.username,
+        });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { profile: profile._id } },
+          { new: true }
+        );
+        return profile;
+      }
+      throw new AuthenticationError();
+    },
+    deleteUser: async (parent, { username }, context) => {
+      if (context.user) {
+        const userData = await User.deleteOne({
+          _id: context.user._id,
+        });
+        await Profile.deleteMany({ username: username });
+        return userData;
+      }
+      throw new AuthenticationError('No user found !');
+    },
+    deleteProfile: async (parent, { _id }) => {
+      const profile = await Profile.findOne({ _id });
+
+      if (profile) {
+        await Profile.deleteOne({ _id });
+
+        return console.log('Profile has been removed !');
+      }
+      throw new AuthenticationError('No profile found !');
+    },
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          args,
+          { new: true }
+        )
+          .select('-__v -password')
+          .populate('profile');
+      }
+    },
+    updateProfile: async (parent, args) => {
+      const profile = await Profile.findOneAndUpdate({ _id: args._id }, args, {
+        new: true,
+      });
+
+      if (profile) {
+        return console.log('Profile has been updated !');
+      }
+
+      throw new AuthenticationError('No profile found !');
+    },
+  },
+};
+
+module.exports = resolvers;
