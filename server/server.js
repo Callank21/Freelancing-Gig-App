e44d067775dea352571a2f00b98e7185999c196f
Seungjin <infinissible@gmail.com>
2022-07-27 20:57:51 -0700

    change path

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
