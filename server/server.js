446eb1762a6250e96dde286f86f9fdcee1527048
Seungjin <infinissible@gmail.com>
2022-07-27 20:27:46 -0700

    change path

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 //Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build/')));
+  app.use(express.static(path.join(__dirname, '../client/public/')));
 }
 
 app.get('*', (req, res) => {
