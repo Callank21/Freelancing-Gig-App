d11d93ba880b7c16326a94dc65fb1bc65e5082d4
Seungjin <infinissible@gmail.com>
2022-07-27 20:56:33 -0700

    build

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 //Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build/')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
