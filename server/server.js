8689f99c196c387f98e9c890d2cf181b075deab7
Seungjin <infinissible@gmail.com>
2022-07-27 21:51:08 -0700

    seed

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { User, Profile } = require('./models');
 const { typeDefs, resolvers } = require('./schema');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const profileSeeds = require('./seeders/profileSeed.json');
-const userSeeds = require('./seeders/userSeed.json');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
